1e26aee49e7b20955b1266bea336b8bcf1433d54
Asdergh <ramzanhac2005@gmail.com>
2024-11-04 22:16:10 +0300

    new model

---
+++ model_1.py
@@ -0,0 +1,252 @@
+import numpy as np
+import tensorflow as tf
+import torch as th
+import matplotlib.pyplot as plt
+import os
+plt.style.use("dark_background")
+
+from tensorflow.keras.layers import Input, Multiply, Dense, Activation, Conv2D, UpSampling2D, MaxPool2D, GlobalAveragePooling2D, Concatenate, Dense, BatchNormalization, Flatten
+from tensorflow.keras import Model, Sequential
+from tensorflow.keras.metrics import Mean
+from tensorflow import Module, GradientTape
+from tensorflow.keras.callbacks import Callback
+
+
+
+class BottleNeck2D(Module):
+
+    def __init__(self, filters, kernel_size=2, l_n=3):
+
+        conv = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")
+        conv1 = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")
+        conv2 = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")
+        con = Concatenate()
+
+        self.layers = [conv, conv1, conv2, con]
+    
+    def __call__(self, input):
+
+        x = input
+        for layer in self.layers[:-1]:
+            x = layer(x)
+        
+        x = self.layers[-1]([input, x])
+        return x
+
+class SeMap2D(Module):
+
+    def __init__(self, ch, ratio=16):
+
+        gap = GlobalAveragePooling2D()
+        se_map = Dense(units=(ch // ratio), activation="relu")
+        linear_gate = Dense(units=ch, activation="softmax")
+        out = Multiply()
+
+        self.layers = [gap, se_map, linear_gate, out]
+    
+    def __call__(self, input):
+
+        x = input
+        for layer in self.layers[:-1]:
+            x = layer(x)
+        
+        x = self.layers[-1]([input, x])
+        return x
+
+class DownSample(Module):
+
+    def __init__(self, filters, kernel_size, activation="silu"):
+
+        conv = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")
+        norm = BatchNormalization()
+        pool = MaxPool2D(pool_size=2)
+        acti = Activation(activation)
+
+        self.layers = [conv, norm, pool, acti] 
+
+    def __call__(self, input):
+
+        x = input
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+
+
+class UpSample(Module):
+
+    def __init__(self, filters, kernel_size, activation="silu"):
+
+        conv = Conv2D(filters=filters, kernel_size=kernel_size, strides=1, padding="same")
+        norm = BatchNormalization()
+        up = UpSampling2D()
+        acti = Activation(activation)
+
+        self.layers = [conv, norm, up, acti]
+    
+    def __call__(self, input):
+
+        x = input
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+
+class Encoding2D(Module):
+
+    def __init__(self, filters, kernel_size, activation):
+
+        bottle_neck = BottleNeck2D(filters=filters, kernel_size=kernel_size)
+        down_sample = DownSample(filters=filters, kernel_size=kernel_size, activation=activation)
+        #se_map = SeMap2D(ch=filters, ratio=16)
+
+        self.layers = [
+            bottle_neck, 
+            down_sample
+            ]
+    
+    def __call__(self, inputs):
+
+        x = inputs
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+
+class Decoding2D(Module):
+
+    def __init__(self, filters, kernel_size, activation):
+
+        bottle_neck = BottleNeck2D(filters=filters, kernel_size=kernel_size)
+        up_sample = UpSample(filters=filters, kernel_size=kernel_size, activation=activation)
+        #se_map = SeMap2D(ch=filters, ratio=16)
+
+        self.layers = [
+            bottle_neck, 
+            up_sample
+            ]
+    
+    def __call__(self, inputs):
+
+        x = inputs
+        for layer in self.layers:
+            x = layer(x)
+        
+        return x
+
+
+
+
+class AeModelCallback(Callback):
+
+
+    def __init__(self, run_folder, model, data, input_sh=(128, 128, 3), sp_save=25, **kwargs):
+        
+        super().__init__(**kwargs)
+        self.model_gen = model
+        self.input_sh = input_sh
+        self.data = data
+        self.sp_save = sp_save
+        self.run_folder = run_folder
+        self.sample_n = 0
+    
+    def on_epoch_end(self, epoch, logs=None):
+
+        rd_idx = np.random.randint(0, self.data.shape[0], self.sp_save)
+        batch = self.data[rd_idx]
+        gen = self.model_gen.predict(batch)
+
+        spr = int(np.sqrt(self.sp_save))
+        fig, axis = plt.subplots()
+        show_pipline = np.zeros(shape=(
+            spr * self.input_sh[0],
+            spr * self.input_sh[1],
+            self.input_sh[-1]
+        ))
+
+        sn = 0
+        for i in range(spr):
+            for j in range(spr):
+
+                show_pipline[i * self.input_sh[0]: (i + 1) * self.input_sh[0],
+                             j * self.input_sh[1]: (j + 1) * self.input_sh[1], 
+                             :] = gen[sn]
+                sn += 1
+        
+        fig.savefig(fname=os.path.join(self.run_folder, f"gen_sample{self.sample_n}.png"))
+        self.sample_n += 1
+
+
+    def on_train_end(self, logs=None):
+
+        weigths_path = os.path.join(self.run_folder, "model.weights.h5")
+        model_path = os.path.join(self.run_folder, "model.keras")
+
+        self.mode_genl.save_weights(weigths_path)
+        self.model_gen.save(model_path)
+
+        
+class AeVersion1(Model):
+
+    def __init__(self, input_sh, **kwargs):
+
+        super().__init__(**kwargs)
+        self.input_sh = input_sh
+        self.model = self._build_model()
+    
+    def _build_model(self):
+
+        input_layer = Input(shape=self.input_sh)
+
+        en_layer = Encoding2D(filters=128, kernel_size=3, activation="tanh")(input_layer)
+        en_layer = Encoding2D(filters=64, kernel_size=3, activation="tanh")(en_layer)
+        en_layer = Encoding2D(filters=32, kernel_size=3, activation="tanh")(en_layer)
+
+        dec_layer = Decoding2D(filters=128, kernel_size=3, activation="tanh")(en_layer)
+        dec_layer = Decoding2D(filters=64, kernel_size=3, activation="tanh")(dec_layer)
+        dec_layer = Decoding2D(filters=32, kernel_size=3, activation="tanh")(dec_layer)
+
+        out = Conv2D(filters=self.input_sh[-1], kernel_size=1, strides=1, activation="relu")(dec_layer)
+        return Model(inputs=input_layer, outputs=out)
+    
+    @property
+    def metrics(self):
+
+        return [
+            self.model_loss_tracker
+        ]
+    
+    def compile(self, optimizer, loss_fn):
+
+        super().compile()
+        self.optimizer = optimizer
+        self.loss_fn = loss_fn
+        self.model_loss_tracker = Mean(name="model_loss_tracker")
+    
+    
+    @tf.function
+    def call(self, inputs):
+        return self.model(inputs)
+    
+    def train_step(self, inputs):
+
+        train_sample, labels = inputs
+        with GradientTape() as gr_tape:
+
+            preds = self.model(train_sample)
+            loss = self.loss_fn(labels, preds)
+        
+        tr_vars = self.model.trainable_variables
+        grads = gr_tape.gradient(loss, tr_vars)
+        self.optimizer.apply_gradients(zip(grads, tr_vars))
+        
+        self.model_loss_tracker.update_state(loss)
+        return {
+            "model_loss": self.model_loss_tracker.result()
+        }
+
+
+
+    
+
+        
--- run_model_train.py
+++ run_model_train.py
@@ -4,65 +4,54 @@ import json as js
 import cv2
 
 from model import VarEncoder
+from model_1 import AeVersion1, AeModelCallback
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.losses import MeanSquaredError
+from tensorflow.keras.optimizers import Adam
 
-target_shape = (128, 128, 3)
-json_params = {
-    "input_shape": target_shape,
-    "hiden_dim": 2,
-    "encoder_out_activations": "tanh",
-    "encoder_conv_filters": [32, 32, 64, 64],
-    "encoder_conv_kernel_size": [3, 3, 3, 3],
-    "encoder_conv_strides": [1, 2, 2, 1],
-    "encoder_conv_padding": ["same", "same", "same", "same"],
-    "encoder_conv_activations": ["linear", "linear", "linear", "linear"],
-    "encoder_conv_dropout": [0.56, 0.56, 0.56, 0.56],
-    "encoder_dense_units": [32, 32, 64, 64],
-    "encoder_dense_dropout_rates": [0.56, 0.56, 0.56, 0.56],
-    "encoder_dense_activations": ["linear", "linear", "linear", "linear"],
 
-    "decoder_conv_filters": [32, 32, 64, 64],
-    "decoder_conv_kernel_size": [3, 3, 3, 3],
-    "decoder_conv_strides": [1, 2, 2, 1],
-    "decoder_conv_padding": ["same", "same", "same", "same"],
-    "decoder_conv_activations": ["linear", "linear", "linear", "linear"],
-    "decoder_conv_dropout": [0.56, 0.56, 0.56, 0.56],
-    "decoder_out_activations": "tanh",
 
-    "weights_init": {
-        "mean": 0.01,
-        "stddev": 0.1
-    }
-
+MODELS = {
+    "0.0": VarEncoder,
+    "1.0": AeVersion1
 }
-
 generator = ImageDataGenerator()
 with open("trainer_confs.json", "r") as json_f:
-    gen_confs = js.load(json_f)
+    training_confs = js.load(json_f)
 
 
 gen_data = {}
-for train_part in gen_confs.keys():
+for train_part in training_confs.keys():
     
-    samples = []
-    samples_ph = gen_confs[train_part]
-    for sample_ph in os.listdir(samples_ph):
+    if train_part != "model_version":
+        samples = []
+        samples_ph = training_confs[train_part]
+        for sample_ph in os.listdir(samples_ph):
 
-        image = cv2.imread(os.path.join(samples_ph, sample_ph))
-        image = cv2.resize(image, (128, 128))
-        image = image / 255.0
+            image = cv2.imread(os.path.join(samples_ph, sample_ph))
+            image = cv2.resize(image, (128, 128))
+            image = image / 255.0
 
-        samples.append(image)
+            samples.append(image)
 
-    samples = np.asarray(samples)
-    gen_data[train_part] = samples
-     
+        samples = np.asarray(samples)
+        gen_data[train_part] = samples
+    
+
+
+model = MODELS[training_confs["model_version"]](input_sh=(128, 128, 3))
+callback = AeModelCallback(data=gen_data["train_data_source"], model=model, input_sh=(128, 128, 3), run_folder="model_saves")
+model.compile(optimizer=Adam(learning_rate=0.1), loss_fn=MeanSquaredError())
+model.fit(gen_data["train_data_source"], 
+          gen_data["train_data_source"], 
+          epochs=100, 
+          batch_size=32, 
+          callbacks=[callback])
+
+
 
     
 
 
-print(gen_data["train_data_source"].shape)
-model = VarEncoder(params_json=json_params)
-model.train(train_tensor=gen_data["train_data_source"], train_labels=gen_data["train_data_source"], epochs=4, batch_size=32, run_folder="model_saves", epoch_per_save=1)
 
 
